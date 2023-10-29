378338946e752bf1d432961df4a48cec6ad4c898
PudinDeveloper <diegoalejandropolanco@gmail.com>
2023-10-29 16:21:04 -0600

    Calendly

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_29_195407) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_29_221750) do
+  create_table "indices", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "medical_histories", force: :cascade do |t|
     t.string "diagnosis"
     t.date "consultation_date"
