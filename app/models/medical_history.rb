c6c9c28a00fa9b74db89e8def0c7a7480e115d60
PudinDeveloper <diegoalejandropolanco@gmail.com>
2023-10-29 17:21:41 -0600

    Model presence

--- app/models/medical_history.rb
+++ app/models/medical_history.rb
@@ -1,3 +1,7 @@
 class MedicalHistory < ApplicationRecord
   belongs_to :patient
+  validates :diagnosis, presence: true
+  validates :consultation_date, presence: true
+  validates :notes, presence: true
+  validates :patient_id, presence: true
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,7 @@
 class Patient < ApplicationRecord
   has_many :medical_histories, dependent: :destroy
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :birthday, presence: true
+  validates :email, presence: true
 end
